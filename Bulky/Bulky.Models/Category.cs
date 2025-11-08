cdad5996015251f6a48996caf8d7b05a65ec0f6c
tomach45 <y.petbottle.t@gmail.com>
2025-11-08 15:17:23 +0900

    関数追加、ver1

--- Bulky/Bulky.Models/Category.cs
+++ Bulky/Bulky.Models/Category.cs
@@ -14,5 +14,11 @@ namespace BulkyBook.Models
         [Range(1, 100, ErrorMessage = "Display Order must be between 1-100")]
         [DisplayName("Order_Ver6")]
         public int DisplayOrder { get; set; }
+
+        public void ver1()
+        {
+            string ver1 = string.Empty;
+            Name_Ver7 = ver1;
+        }
     }
 }
