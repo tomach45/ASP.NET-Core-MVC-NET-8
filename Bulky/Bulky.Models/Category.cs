8ad4be4b30999d124c8ba0cdd601b4b7f054df55
tomach45 <y.petbottle.t@gmail.com>
2025-11-08 15:55:32 +0900

    ver2追加

--- Bulky/Bulky.Models/Category.cs
+++ Bulky/Bulky.Models/Category.cs
@@ -26,5 +26,11 @@ namespace BulkyBook.Models
             string ver1 = string.Empty;
             Name_Ver7 = ver1;
         }
+
+        public void ver2()
+        {
+            string ver2 = string.Empty;
+            Name_Ver7 = ver2;
+        }
     }
 }
