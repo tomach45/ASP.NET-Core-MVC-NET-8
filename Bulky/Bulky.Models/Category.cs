c1130c57cae46e686f4e84896724cdb78f0c6f3d
tomach45 <y.petbottle.t@gmail.com>
2025-12-14 20:46:41 +0900

    マージ原本

--- Bulky/Bulky.Models/Category.cs
+++ Bulky/Bulky.Models/Category.cs
@@ -10,33 +10,15 @@ namespace BulkyBook.Models
         [Required]
         [MaxLength(30)]
         [DisplayName("Name_Ver6")]
-        public string Name_Ver7 { get; set; }
+        public string Name_Ver1 { get; set; }
         [Range(1, 100, ErrorMessage = "Display Order must be between 1-100")]
         [DisplayName("Order_Ver6")]
         public int DisplayOrder { get; set; }
 
-        public void Genpon()
-        {
-            string genpon = string.Empty;
-            Name_Ver7 = genpon;
-        }
-
         public void ver1()
         {
             string ver1 = string.Empty;
-            Name_Ver7 = ver1;
-        }
-
-        public void Genpon2()
-        {
-            string genpon2 = string.Empty;
-            Name_Ver7 = genpon2;
-        }
-
-        public void ver2()
-        {
-            string ver2 = string.Empty;
-            Name_Ver7 = ver2;
+            Name_Ver1 = ver1;
         }
     }
 }
