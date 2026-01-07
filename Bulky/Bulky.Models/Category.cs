28542468c7d07067a0298ab3d1354576372a5f34
tomach45 <y.petbottle.t@gmail.com>
2026-01-08 00:12:04 +0900

    自分の修正（work_Yamashita2）

--- Bulky/Bulky.Models/Category.cs
+++ Bulky/Bulky.Models/Category.cs
@@ -26,7 +26,7 @@ namespace BulkyBook.Models
             string ver2 = string.Empty;
             Name_Ver1 = "dakjbfdskbfis";
             string ver3 = string.Empty;
-            Name_Ver1 ="自分の修正";
+            Name_Ver1 ="自分の修正（work_Yamashita2）";
         }
     }
 }
